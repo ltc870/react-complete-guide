9b51d43ffdea19b555e942b8cd1867e3a3477e1b
Lawrence C <lawrencecaudle870@gmail.com>
2022-11-05 15:02:48 -0400

    added functions to clear userInput

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -29,6 +29,9 @@ const ExpenseForm = () => {
             date: enteredDate,
         };
         console.log(expenseData);
+        setEnteredTitle("");
+        setEnteredAmount("");
+        setEnteredDate("");
     };
 
     return (
@@ -36,12 +39,17 @@ const ExpenseForm = () => {
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>{enteredTitle}</label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input
+                        type="text"
+                        value={enteredTitle}
+                        onChange={titleChangeHandler}
+                    />
                 </div>
                 <div className="new-expense__control">
                     <label>{enteredAmount}</label>
                     <input
                         type="number"
+                        value={enteredAmount}
                         min="0.01"
                         step="0.01"
                         onChange={amountChangeHandler}
@@ -51,6 +59,7 @@ const ExpenseForm = () => {
                     <label>{enteredDate}</label>
                     <input
                         type="date"
+                        value={enteredDate}
                         min="2019-01-01"
                         max="2022-12-31"
                         onChange={dateChangeHandler}
