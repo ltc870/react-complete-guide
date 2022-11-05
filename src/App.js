1262297cb8bffc77d346a4075f769670da2d99c3
Lawrence C <lawrencecaudle870@gmail.com>
2022-11-05 15:25:01 -0400

    child to parent component communication

--- src/App.js
+++ src/App.js
@@ -28,9 +28,14 @@ const App = () => {
             date: new Date(2021, 5, 12),
         },
     ];
+
+    const addExpenseHandler = (expenses) => {
+        console.log("In App.js");
+        console.log(expenses);
+    };
     return (
         <div>
-            <NewExpense />
+            <NewExpense onAddExpense={addExpenseHandler} />
             <Expenses expenses={expenses} />
         </div>
     );
